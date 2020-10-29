55b6c57c200fb2cf972ceae4a3082ba50d977401
Ilya <softclubplus@gmail.com>
2020-10-29 10:56:29 +0300

    node seed DB with mock data MongoDB connection and class

---
+++ node_api/developDB/rentalsMockData.js
@@ -0,0 +1,74 @@
+export const rentals = [
+  {
+    title: 'Spacious Apartment',
+    city: 'St-Petersburg',
+    street: 'Morskaya',
+    category: 'apartment',
+    numOfRooms: 2,
+    numOfGuests: 4,
+    numOfBeds: 3,
+    shared: true,
+    image:
+      'http://expatflat.ru/wp-content/uploads/2020/01/68773238_2928487323844974_7867738448222748672_n.jpg?v=1579972022',
+    image2: 'https://ycdn.space/h/2016/07/006-apartment-moscow.webp',
+    dailyPrice: 90,
+    description: `In the Tsentralny district of Saint Petersburg, close to Anna
+    Akhmatova Museum, Peter the Great Apartments on Rubinshteyna features
+    free WiFi and a washing machine. The property has city views and is
+    1.9 km from Faberge Museum and 2 km from State Russian Museum.`,
+  },
+  {
+    title: 'Modern-looking flat',
+    city: 'Moscow',
+    street: 'Izmaylovskaya',
+    category: 'apartment',
+    numOfRooms: 2,
+    numOfGuests: 2,
+    numOfBeds: 1,
+    shared: true,
+    image:
+      'https://a0.muscache.com/im/pictures/9ab04870-60f4-4ad8-a084-14e1ffe2b746.jpg?im_w=1200',
+    image2:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTM2in1IKoLEKdu_IkFm9lJbuGeRKa8QOCcAQ&usqp=CAU',
+    dailyPrice: 60,
+    description: `In the green surroundings of the Izmailovsky Park,
+    this hotel provides you with elegant rooms. Izmailovo Alfa Hotel is located in a 2 minutes' walk from Izmailovo MCC.
+    It is 50 m from Partisanskaya Metro Station, a 15-minute ride from central Moscow.
+    Wi-Fi is free in all parts of the Izmailovo Alfa Hotel.`,
+  },
+  {
+    title: 'Modern-looking flat',
+    city: 'Moscow',
+    street: 'Tverskaya',
+    category: 'apartment',
+    numOfRooms: 1,
+    numOfGuests: 7,
+    numOfBeds: 4,
+    shared: false,
+    street: 'Sunny Vale Street',
+    image:
+      'https://a0.muscache.com/im/pictures/14fa236e-6d43-48fd-8024-700b316dc47e.jpg?im_w=1200',
+    image2:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRWz3txLEVUu2_H7YcHJhkSymc7VIRsNAsg-w&usqp=CAU',
+    dailyPrice: 40,
+    description: `Welcome to the mini-apartments "New York"! This is a modern compact housing of 14 square meters, located in a new business class residential complex on the banks of the Nagatinsky backwater. Picturesque embankments, the Kolomenskoye Museum-Reserve and the Dream Island theme park`,
+  },
+  {
+    title: 'Spacious Apartment',
+    city: 'St-Petersburg',
+    street: 'Groznyi Boulevard',
+    category: 'apartment',
+    numOfRooms: 2,
+    numOfGuests: 4,
+    numOfBeds: 3,
+    shared: true,
+    image:
+      'http://expatflat.ru/wp-content/uploads/2020/01/68773238_2928487323844974_7867738448222748672_n.jpg?v=1579972022',
+    image2: 'https://ycdn.space/h/2016/07/006-apartment-moscow.webp',
+    dailyPrice: 90,
+    description: `In the Tsentralny district of Saint Petersburg, close to Anna
+    Akhmatova Museum, Peter the Great Apartments on Rubinshteyna features
+    free WiFi and a washing machine. The property has city views and is
+    1.9 km from Faberge Museum and 2 km from State Russian Museum.`,
+  },
+];
