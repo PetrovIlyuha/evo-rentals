23b4158d51dbd8ade9e60a430c8cfa9cf073d5cc
Ilya <softclubplus@gmail.com>
2020-11-06 11:44:56 +0300

    add redux store

---
+++ react-client/src/redux/store.js
@@ -0,0 +1,35 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import {
+  rentalDetailsReducer,
+  rentalsListReducer,
+} from './rentals_slice/rentalsReducer';
+import {
+  loginUserReducer,
+  registerUserReducer,
+} from './user_slice/authReducer';
+
+// reducers
+
+const reducer = combineReducers({
+  rentals: rentalsListReducer,
+  rentalByID: rentalDetailsReducer,
+  userRegister: registerUserReducer,
+  userLogin: loginUserReducer,
+});
+
+const initialState = {
+  rentals: [],
+  rentalByID: {},
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware)),
+);
+
+export default store;
