3e5e2650bdebc303937debc093d1544abe093891
Ilya <softclubplus@gmail.com>
2020-11-07 09:50:38 +0300

    create new rental object redux action

--- react-client/src/redux/rentals_slice/rentalActions.js
+++ react-client/src/redux/rentals_slice/rentalActions.js
@@ -3,6 +3,10 @@ import {
   RENTALS_LIST_FAIL,
   RENTALS_LIST_REQUEST,
   RENTALS_LIST_SUCCESS,
+  RENTAL_CREATE_FAILURE,
+  RENTAL_CREATE_REQUEST,
+  RENTAL_CREATE_RESET,
+  RENTAL_CREATE_SUCCESS,
   RENTAL_DETAILS_FAIL,
   RENTAL_DETAILS_REQUEST,
   RENTAL_DETAILS_SUCCESS,
@@ -37,5 +41,30 @@ export const getRentalById = id => async dispatch => {
           ? err.response.data.message
           : err.message,
     });
+  }
+};
+
+export const createRental = rentalValues => async dispatch => {
+  const token = localStorage.getItem('user-session-token');
+
+  try {
+    dispatch({ type: RENTAL_CREATE_REQUEST });
+    await axios.post(`/api/v1/rentals/`, rentalValues, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    dispatch({ type: RENTAL_CREATE_SUCCESS });
+  } catch (err) {
+    dispatch({
+      type: RENTAL_CREATE_FAILURE,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  } finally {
+    dispatch({ type: RENTAL_CREATE_RESET });
   }
 };
