e36bcecf8ba451a597ddfacbd26800b49fb544a8
Ilya <softclubplus@gmail.com>
2020-11-11 12:22:32 +0300

    adding booking mongoose model with references to rental and user mongo objects

---
+++ node_api/models/bookingModel.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const bookingSchema = new mongoose.Schema({
+  startDate: { type: Date, required: 'Arrival Date is required' },
+  endDate: { type: Date, required: 'Departure Date is required' },
+  totalPrice: { type: Number, require: 'Total price is required' },
+  nights: { type: Number, required: true },
+  guests: { type: Number, required: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  rental: { type: Schema.Types.ObjectId, ref: 'Rental', required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export const Booking = mongoose.model('Booking', bookingSchema);
