2ace5767f9ab65b87aa1c32a05767534b05966d2
Ilya <softclubplus@gmail.com>
2020-11-18 15:28:36 +0300

    main layout container style changes (more engaging background)

--- react-client/src/components/ui_layout/BaseLayout.jsx
+++ react-client/src/components/ui_layout/BaseLayout.jsx
@@ -4,7 +4,16 @@ import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles(theme => ({
-  mainContainer: { marginTop: '8rem' },
+  mainContainer: {
+    paddingTop: '8rem',
+    marginTop: '-2rem',
+    height: '100%',
+    backgroundColor: 'rgba(240, 240, 245,.4)',
+    backgroundImage:
+      'radial-gradient(rgba(68, 76, 247, 0.15) 1px, transparent 1px), radial-gradient(rgba(68, 76, 247, 0.15) 1px, rgba(229, 229, 247, 0.3) 1px)',
+    backgroundSize: '40px 40px',
+    backgroundPosition: '0 0,20px 20px',
+  },
 }));
 
 const BaseLayout = ({ children }) => {
