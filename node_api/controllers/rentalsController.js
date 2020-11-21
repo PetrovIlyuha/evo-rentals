fb640951a410fa56567178e904cdcf2ef1abe779
Ilya <softclubplus@gmail.com>
2020-11-21 14:56:43 +0300

    controller to delete rental posting with authorship check and active booking presence (prohibitive clause)

--- node_api/controllers/rentalsController.js
+++ node_api/controllers/rentalsController.js
@@ -1,4 +1,5 @@
 import { Rental } from '../models/rentalModel.js';
+import { Booking } from '../models/bookingModel.js';
 
 export const getAllRentals = async (req, res) => {
   const { city } = req.query;
@@ -35,7 +36,6 @@ export const getRentalById = async (req, res) => {
 
 export const getRentalOwner = async (req, res) => {
   const placeId = req.params.id;
-  console.log(placeId);
   const rentalWithOwner = await Rental.findById(placeId)
     .populate('owner')
     .select('-_id')
@@ -84,7 +84,37 @@ export const createRental = async (req, res) => {
 
 export const removeRentalById = async (req, res) => {
   const { id } = req.params;
-  await Rental.findByIdAndRemove(id);
+  const { user } = res.locals;
+  try {
+    const rental = await Rental.findById(id).populate('owner');
+    const bookings = await Booking.find({ rental });
+    console.log(bookings);
+    if (user.id !== rental.owner.id) {
+      res.status(403).json({
+        message:
+          'You are not an owner for this rental! Operation has been terminated!',
+      });
+    }
+    if (bookings && bookings.length > 0) {
+      const permittedDeletion = bookings.every(
+        booking => moment(booking.endDate).diff(moment(), 'days') > 0,
+      );
+      if (permittedDeletion) {
+        await rental.remove();
+        return res.json({ id });
+      } else {
+        return res.status(400).json({
+          message: 'Location have active bookings. Operation canceled.',
+        });
+      }
+    }
+    await rental.remove();
+    return res.json({ id });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: "Rental can't be removed! Try again later..." });
+  }
 };
 
 export const updateRentalById = async (req, res) => {
